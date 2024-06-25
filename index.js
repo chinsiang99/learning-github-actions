8ccf82f17f1da62aa7b7615f70504f6048a7f94b
Tan Chin Siang <chinsiang99@gmail.com>
2024-06-25 16:39:36 +0800

    feat: create another endpoint for demo purpose

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
   res.send('Welcome to my server!');
 });
 
+app.get('/about', (req, res) => {
+  res.send('Currently learning CI/CD');
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
